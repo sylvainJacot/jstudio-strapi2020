c00b3b83642e6a1202dfe1d5020b09c3df5d53f8
coatriver <sylvain.jacot@hotmail.be>
2020-11-07 07:13:55 +0100

    test

--- src/components/01 Atoms/Clients.js
+++ src/components/01 Atoms/Clients.js
@@ -14,31 +14,27 @@ import HiltonLogo from "../../media/icons/clients/Hilton.svg";
 import WTFLogo from "../../media/icons/clients/wtf.png";
 import SheratonLogo from "../../media/icons/clients/Sheraton.svg";
 
-
 export const ClientsUXUI = [
-    {altimg: "Samsung client logo", src: SamsungLogo},
-    {altimg: "Kia client logo", src: KiaLogo},
-    {altimg: "Love the garden client logo", src: LoveTheGardenLogo},
-    {altimg: "Mazda client logo", src: MazdaLogo},
-    {altimg: "Kit Cross client logo", src: KitCrossLogo},
-    {altimg: "Telenet client logo", src: TelenetLogo},
-]
-
+  { altimg: "Samsung client logo", src: SamsungLogo },
+  { altimg: "Kia client logo", src: KiaLogo },
+  { altimg: "Love the garden client logo", src: LoveTheGardenLogo },
+  { altimg: "Mazda client logo", src: MazdaLogo },
+  { altimg: "KitCross client logo", src: KitCrossLogo },
+  { altimg: "Telenet client logo", src: TelenetLogo },
+];
 
 export const ClientsDeveloper = [
-    {altimg: "Clavie client logo", src: ClavieLogo},
-    {altimg: "Parentreprise client logo", src: ParentrepriseLogo},
-    {altimg: "So Smart client logo", src: SoSmartLogo},
-    {altimg: "Love the garden client logo", src: LoveTheGardenLogo},
-
-]
+  { altimg: "Clavie client logo", src: ClavieLogo },
+  { altimg: "Parentreprise client logo", src: ParentrepriseLogo },
+  { altimg: "So Smart client logo", src: SoSmartLogo },
+  { altimg: "Love the garden client logo", src: LoveTheGardenLogo },
+];
 
 export const ClientsRetoucher = [
-    {altimg: "Fred Sablon Photographer logo", src: FredSablonLogo2},
-    {altimg: "Clio Golbrenner client logo", src: ClioGolbrennerLogo},
-    {altimg: "Eat Brussels client logo", src: EatBxlLogo},
-    {altimg: "Hilton hotel client logo", src: HiltonLogo},
-    {altimg: "What The Fun Comedy CLub client logo", src: WTFLogo},
-    {altimg: "Sheraton Hotels and resorts client logo", src: SheratonLogo},
-]
-
+  { altimg: "Fred Sablon Photographer logo", src: FredSablonLogo2 },
+  { altimg: "Clio Golbrenner client logo", src: ClioGolbrennerLogo },
+  { altimg: "Eat Brussels client logo", src: EatBxlLogo },
+  { altimg: "Hilton hotel client logo", src: HiltonLogo },
+  { altimg: "What The Fun Comedy CLub client logo", src: WTFLogo },
+  { altimg: "Sheraton Hotels and resorts client logo", src: SheratonLogo },
+];
